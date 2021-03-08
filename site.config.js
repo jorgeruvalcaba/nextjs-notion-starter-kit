990b3e9f1b30ba30b6c0ac6a3e7d242558149ba0
Jorge Ruvalcaba <jorgeruvalcabav@gmail.com>
2021-03-08 11:37:15 -0600

    Adding fix for footer icons after rehydration (#1)

--- site.config.js
+++ site.config.js
@@ -1,38 +1,38 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'cce3a9ef1daa4924b0e6f02efea91121',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Tabs & Spaces',
+  domain: 'jorgeruvalcaba.dev',
+  author: 'Jorge Ruvalcaba',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  socialImageTitle: 'Jorge Ruvalcaba',
+  socialImageSubtitle: 'Tabs & Spaces',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'jorgearuv',
+  github: 'jorgeruvalcaba',
+  linkedin: 'jorge-ruvalcaba',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
-  defaultPageCoverPosition: 0.5,
+  defaultPageCoverPosition: 0.1862,
 
   // image CDN host to proxy all image requests through (optional)
   // NOTE: this requires you to set up an external image proxy
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: null,
+  utterancesGitHubRepo: 'jorgeruvalcaba/personal-site-v3',
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
