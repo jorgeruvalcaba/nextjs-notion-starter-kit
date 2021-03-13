c62753d6871479411e043d76ad3ac8727719f12c
Jorge Ruvalcaba <jorgeruvalcabav@gmail.com>
2021-03-13 12:48:46 -0600

    Adding GA setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,8 +37,9 @@ import 'prismjs/components/prism-bash'
 import React from 'react'
 import { useRouter } from 'next/router'
 import { bootstrap } from 'lib/bootstrap-client'
-import { fathomId, fathomConfig } from 'lib/config'
-import * as Fathom from 'fathom-client'
+// import { fathomId, fathomConfig } from 'lib/config'
+// import * as Fathom from 'fathom-client'
+import * as gtag from '../lib/gtag'
 
 if (typeof window !== 'undefined') {
   bootstrap()
@@ -48,20 +49,18 @@ export default function App({ Component, pageProps }) {
   const router = useRouter()
 
   React.useEffect(() => {
-    if (fathomId) {
-      Fathom.load(fathomId, fathomConfig)
-
-      function onRouteChangeComplete() {
-        Fathom.trackPageview()
+    if (gtag.gaId) {
+      const handleRouteChange = (url) => {
+        gtag.pageview(url)
       }
 
-      router.events.on('routeChangeComplete', onRouteChangeComplete)
+      router.events.on('routeChangeComplete', handleRouteChange)
 
       return () => {
-        router.events.off('routeChangeComplete', onRouteChangeComplete)
+        router.events.off('routeChangeComplete', handleRouteChange)
       }
     }
-  }, [])
+  }, [router.events])
 
   return <Component {...pageProps} />
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,6 +35,24 @@ export default class MyDocument extends Document {
             />
 
             <link rel='manifest' href='/manifest.json' />
+
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                page_path: window.location.pathname,
+              });
+          `
+              }}
+            />
           </Head>
 
           <body>
